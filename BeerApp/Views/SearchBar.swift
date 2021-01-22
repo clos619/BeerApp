cb2cea02db5a098c106b01ad04f88e1d6adc7ec1
mshirichena <74990273+mshirichena@users.noreply.github.com>
2021-01-21 21:03:09 -0500

    SearchBar
    1.1st iteration added SearchBar.
    //TODOS add capability to search using (1. id 2. name 3. street address 4. region(state) 5. postal code 6. brewery id 7. brewery name 8. name short display 9. description 10. year established  11. icon (image) 12. Brewery Image)

---
+++ BeerApp/Views/SearchBar.swift
@@ -0,0 +1,62 @@
+//
+//  SearchBar.swift
+//  BeerApp
+//
+//  Created by Christian Shirichena on 1/21/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct SearchBar: View {
+    @Binding var text: String
+    @State private var isEditing = false
+    
+    var body: some View {
+        HStack {
+            TextField("Search for a Beer", text: $text)
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray))
+                .cornerRadius(8)
+                // Search icon and cross icon
+                .overlay(HStack {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(.gray)
+                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity,  alignment: .leading)
+                        .padding(.leading, 8)
+                    
+                    if isEditing {
+                        Button(action: {
+                            self.text = ""
+                        }) {
+                            Image(systemName: "multiply.circle.fill")
+                                .foregroundColor(.gray)
+                                .padding(.trailing, 8)
+                        }
+                    }
+                })
+                .padding(.horizontal, 10)
+                .onTapGesture {
+                    self.isEditing = true
+                }
+            if isEditing {
+                Button(action: {
+                    self.isEditing = false
+                    self.text = ""
+                }) {
+                    Text("Cancel")
+                }
+                .padding(.trailing,10)
+                .transition(.move(edge: .trailing))
+                .animation(.default)
+            }
+        }
+    }
+}
+
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBar(text: .constant(""))
+    }
+}
