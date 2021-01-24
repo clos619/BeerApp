5bfc225c3181cc5227015adf6e7827fd8de984e1
Carlos Henderson <chenderson6191@gmail.com>
2021-01-24 11:37:59 -0800

    changes made to searchbar

--- BeerApp/Views/HomeView.swift
+++ BeerApp/Views/HomeView.swift
@@ -15,12 +15,16 @@ struct HomeView: View {
     }
     private let database = Database.database().reference()
     var body: some View {
+        
+        HStack{
+            SearchBar(userEnteredZipCode: userEnteredZipCode)
+        }
         NavigationView{
         Text("Welcome Home")
-        Label{
-            Text("User: \(00)")
-        }icon:{
-        }
+//        Label{
+//            //Text("User: \(00)")
+//        }icon:{
+//        }
             VStack{
                 HStack{
                     Button(action: session.signOut){
